ad361e394d3df0456e7c6380ae2093983f1ebd56
EcarterVS <99089807+EcarterVS@users.noreply.github.com>
2022-02-05 12:58:51 -0600

    updated after reading rubric

--- script.js
+++ script.js
@@ -6,13 +6,30 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years)
 
+    if(principal == ""){
+        alert("Please enter positive principal amount");
+        return false;
+    }
 
+    if(principal < 0){
+        alert("Principal amount cannot be negative");
+        return false;
+    }
 
-    document.getElementById("result").innerHTML= "If you deposit:" + principal + ",\<br/> at an interest rate of: " + rate + "%. \<br/> You will receive an amount of: " + interest + "\<br/>in the year " + year + ".";
-
-
+    document.getElementById("result").innerHTML= "If you deposit: " +  principal + ",\<br/> at an interest rate of: " +  rate   + "%. \<br/> You will receive an amount of: " + interest + "\<br/>in the year " + year + ".";
 }
 function updateRate(){
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerHTML = rateval;
-}     
+    document.getElementById("rate_val").innerHTML = rateval + "%";
+}
+
+function keepPositive(){
+    var principal = document.getElementById("principal").value;
+    if(principal<0){
+        document.getElementById("principal").value = 0;
+        document.getElementById("principal").focus();
+        document.getElementById("keepPositive").innerHTML = "Please enter a positive number";
+
+
+    }
+}
